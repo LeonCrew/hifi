bff18d8aeb675e675213cf2e75c324c792eb0cad
Alexandr Murashko <a.murashko@gamedevsource.com>
2017-05-18 00:14:20 +0300

    Add custom activity before stereo view

--- interface/src/java/io/highfidelity/hifiinterface/InterfaceActivity.java
+++ interface/src/java/io/highfidelity/hifiinterface/InterfaceActivity.java
@@ -11,10 +11,14 @@
 
 package io.highfidelity.hifiinterface;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.util.Log;
 import org.qtproject.qt5.android.bindings.QtActivity;
@@ -23,14 +27,19 @@ import com.google.vr.cardboard.DisplaySynchronizer;
 import com.google.vr.cardboard.DisplayUtils;
 import com.google.vr.ndk.base.GvrApi;
 import android.graphics.Point;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class InterfaceActivity extends QtActivity {
-    
+
     public static native void handleHifiURL(String hifiURLString);
     private native long nativeOnCreate(AssetManager assetManager, long gvrContextPtr);
     private native void saveRealScreenSize(int width, int height);
 
     private AssetManager assetManager;
+
+    private TextView tv = null;
 
     // Opaque native pointer to the Application C++ object.
     // This object is owned by the InterfaceActivity instance and passed to the native methods.
@@ -40,7 +49,25 @@ public class InterfaceActivity extends QtActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        
+
+//        tv = new TextView(this);
+//        tv.setText("TEXT");
+//        tv.setLayoutParams(new LinearLayout.LayoutParams(
+//                LinearLayout.LayoutParams.FILL_PARENT,
+//                LinearLayout.LayoutParams.FILL_PARENT));
+//
+//        tv.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                ((ViewGroup)tv.getParent()).removeView(tv);
+//                tv = null;
+//            }
+//        });
+//
+//        setContentView(tv);
+
+
         // Get the intent that started this activity in case we have a hifi:// URL to parse
         Intent intent = getIntent();
         if (intent.getAction() == Intent.ACTION_VIEW) {
@@ -63,5 +90,17 @@ public class InterfaceActivity extends QtActivity {
         Point size = new Point();
         getWindowManager().getDefaultDisplay().getRealSize(size);
         saveRealScreenSize(size.x, size.y);
+
+        //Intent intent2 = new Intent(this, SwitchActivity.class);
+        //startActivity(intent2);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent2 = new Intent(InterfaceActivity.this, SwitchActivity.class);
+                startActivity(intent2);
+            }
+        }, 3000);
     }
 }
